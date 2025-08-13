9a8b21c95511c7a73ccee4f2ac31dde64872ecc5
Taisa Braga <taisabraga.adm@gmail.com>
2025-08-13 19:00:40 +0100

    fix import address

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,7 +1,7 @@
 
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import translatePageEN from "../translations/English/TranslatePageEN.json"
+import translatePageEN from "../translations/English/translatePageEN.json"
 import translatePagePT from "../translations/Portuguese/translatePagePT.json"
 
 i18n.use(initReactI18next).init({
